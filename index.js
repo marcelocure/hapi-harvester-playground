42cea5f940360a37819270f0bf0712c1c27a57c6
Marcelo Cure <marcelo.cure@ilegra.com>
2016-04-21 18:34:30 -0300

    Adding event creation

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const Hapi = require('hapi'),
     require_dir = require('require-directory'),
     _ = require('underscore'),
     adapter = hapiHarvester.getAdapter('mongodb'),
-    server = new Hapi.Server({});
+    server = new Hapi.Server({}),
+    mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/testing');
+
+var promotionEvent = mongoose.model('promotionEvents', { id: String });
 
 server.connection({port: config.port});
 server.register([ //{register: require('hapi-swagger'), options: {apiVersion: require('./package.json').version}},
@@ -14,9 +19,29 @@ server.register([ //{register: require('hapi-swagger'), options: {apiVersion: re
         server.start(() => loadResources(server, harvester))
     });
 
+server.route({
+   method: 'POST',
+   path:'/brands/{id}/promote', 
+   handler: function (request, reply) {
+       storeEvent(encodeURIComponent(request.params.id));
+       return reply('hello world');
+   }
+});
+
 function loadResources(server, harvester) {
     var models = require_dir(module, './models');
     _.map(Object.keys(models), function(model){
         server.route(models[model](harvester));
+    });
+}
+
+function storeEvent(id) {
+    var promote = new promotionEvent({ id: id });
+    promote.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('ok');
+      }
     });
 }
